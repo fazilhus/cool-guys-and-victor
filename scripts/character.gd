6516e85b6ccad3dcf5e56ecdf4670716a3e9a034
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2024-11-22 13:22:54 +0100

    base character with gealth component

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,2 +1,15 @@
 extends Node2D
 class_name Character
+
+@onready var stats_comp = %Stats
+
+signal character_health_reached_zero
+
+func _ready() -> void:
+	stats_comp.health_reached_zero.connect(on_health_reached_zero())
+
+func take_damage(dmg: int) -> void:
+	stats_comp.take_damage(dmg)
+
+func on_health_reached_zero():
+	character_health_reached_zero.emit()
