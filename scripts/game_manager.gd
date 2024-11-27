58f4e10af7281cb2f515013674fe7f03f39d5ea7
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2024-11-27 12:53:32 +0100

    less error-prone turn phase system

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -29,26 +29,62 @@ func on_batlle_end() -> void:
 	current_turn_phase = Enum.TurnPhase.None
 
 func process_turn_phase_start() -> void:
+	if current_turn_phase != Enum.TurnPhase.None && current_turn_phase != Enum.TurnPhase.End:
+		print(
+			"[ERROR] trying to start turn phase START out of order: ",
+			Util.turn_phase_to_str(current_turn_phase))
+		return
+	
 	current_turn_phase = Enum.TurnPhase.Start
 	turn_phase_start.emit()
 	# wait for start of the turn to be processed
 	start_turn_phase_player()
 
 func start_turn_phase_player() -> void:
+	if current_turn_phase != Enum.TurnPhase.Start:
+		print(
+			"[ERROR] trying to start turn phase Player out of order: ",
+			Util.turn_phase_to_str(current_turn_phase))
+		return
+
 	current_turn_phase = Enum.TurnPhase.Player
 	turn_phase_player.emit()
 
 func end_turn_phase_player() -> void:
+	if current_turn_phase != Enum.TurnPhase.Player:
+		print(
+			"[ERROR] trying to end turn phase Player out of order: ",
+			Util.turn_phase_to_str(current_turn_phase))
+		return
+	
 	start_turn_phase_enemy()
 
 func start_turn_phase_enemy() -> void:
+	if current_turn_phase != Enum.TurnPhase.Player:
+		print(
+			"[ERROR] trying to start turn phase Enemy out of order: ",
+			Util.turn_phase_to_str(current_turn_phase))
+		return
+
 	current_turn_phase = Enum.TurnPhase.Enemy
 	turn_phase_enemy.emit()
 
 func end_turn_phase_enemy() -> void:
-	pass
+	if current_turn_phase != Enum.TurnPhase.Enemy:
+		print(
+			"[ERROR] trying to end turn phase Enemy out of order: ",
+			Util.turn_phase_to_str(current_turn_phase))
+		return
+
+	process_turn_phase_end()
 
 func process_turn_phase_end() -> void:
+	if current_turn_phase != Enum.TurnPhase.Enemy:
+		print(
+			"[ERROR] trying to start turn phase End out of order: ",
+			Util.turn_phase_to_str(current_turn_phase))
+		return
+
 	current_turn_phase = Enum.TurnPhase.End
 	turn_phase_end.emit()
 	# wait for end of the turn to be processed
